f56fff47c45697b5ffca5c34c380b2119d82fb10
Thomas MAC <tcarbillet@brief.me>
2024-09-17 15:54:21 +0200

    feat(admin): add uai in search_fileds

--- french_highschool/admin.py
+++ french_highschool/admin.py
@@ -6,6 +6,7 @@ from .models import School
 @admin.register(School)
 class SchoolAdmin(admin.ModelAdmin):
     list_display = ("uai", "__str__")
+    search_fields = ("uai",)
 
     def has_change_permission(self, request, obj=None):
         return False
